a7e0db87fe53f276164c2357e2c5dc04b6458786
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-06-06 15:07:24 -0400

    Initial commit

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra/reloader'
+
+configure :development, :test do
+  require 'pry'
+end
+
+configure do
+  set :views, 'app/views'
+end
+
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
+  require file
+  also_reload file
+end
+
+get '/' do
+  @title = "Hello World"
+  erb :index
+end
